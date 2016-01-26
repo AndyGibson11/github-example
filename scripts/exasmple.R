54a04c0065449eb36722a95e72651402ad608c02
Andy <andy.gibson11@gmail.com>
2016-01-26 15:54:00 +0000

    test1

--- scripts/exasmple.R
+++ scripts/exasmple.R
@@ -7,3 +7,10 @@ example_data<-read.csv(file.path(data.path,"example.csv"))
 
 
 plot(rnorm(100,sd=5)+1:100,1:100)
+
+x <- matrix(data = c(1:9), nrow = 3, ncol = 3)
+
+y <-matrix (data=2, nrow = 3, ncol = 3)
+
+rbind(x, y)
+cbind(x, y)
